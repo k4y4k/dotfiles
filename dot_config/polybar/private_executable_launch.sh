5f0caf3b11fcfdda6660a12505a83ebc3bc58221
kayak <kayak@kayak.rocks>
2021-10-29 13:09:18 +1000

    polybar: add config + spotify

---
+++ dot_config/polybar/private_executable_launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# 🤷
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
