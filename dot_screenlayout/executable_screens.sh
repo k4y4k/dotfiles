107760a8bc1711afef57e2bc42dfa893cb39190b
kayak <kayak@kayak.rocks>
2024-06-20 13:08:44 +1000

    i3: now dvorak is my best friend

--- dot_screenlayout/executable_screens.sh
+++ dot_screenlayout/executable_screens.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP1 --off --output HDMI1 --mode 1680x1050 --pos 2970x630 --rotate normal --output HDMI2 --mode 1920x1080 --pos 0x600 --rotate normal --output HDMI3 --mode 1680x1050 --pos 1920x0 --rotate left --output VIRTUAL1 --off
+xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --mode 3440x1440 --pos 1920x0 --rotate normal --output DP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x540 --rotate normal
