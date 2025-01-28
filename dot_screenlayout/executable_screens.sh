67c0d5fcac6d860d9bb665673833e902aeda3e6e
kayak <kayak@kayak.rocks>
2025-01-28 10:25:07 +1000

    arandr: i dont have 2 screens anymore

--- dot_screenlayout/executable_screens.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --mode 3440x1440 --pos 1920x0 --rotate normal --output DP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x608 --rotate normal
-
