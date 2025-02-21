36ccc7db5966c069483ed4b66d3effeea6384288
kayak <kayak@kayak.rocks>
2025-02-21 11:45:08 +1000

    wip(wezterm): my light theme does not spark joy...

--- dot_config/wezterm/background.lua
+++ dot_config/wezterm/background.lua
@@ -26,7 +26,7 @@ function module.choose_bg_image()
 
       {
         source = { Color = "#fff" },
-        opacity = 0.5,
+        opacity = 0.7,
         width = "100%",
         height = "100%",
       },
